fdd3a650d6bff5d9c4fc3f07d9f0fbaa6a227f26
Ajay Dudani <adudani@codeaurora.org>
2011-09-18 19:05:42 -0700

    msm8960: Declare support for sensor hardware available on Fluid device
    Change-Id: I16ccdc303944e6b828bf93e919bde5e4e0a86ab2

--- msm8960.mk
+++ msm8960.mk
@@ -7,3 +7,11 @@ PRODUCT_DEVICE := msm8960
 PRODUCT_PROPERTY_OVERRIDES += \
  ro.com.google.clientidbase=android-qualcomm
 
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.barometer.xml:system/etc/permissions/android.hardware.sensor.barometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/base/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+
