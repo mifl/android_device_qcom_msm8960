f8641054f6f8808309887524d004828b41d333c5
Kun Han Kim <kunhank@codeaurora.org>
2011-11-18 12:24:32 -0800

    Bluetooth: Add bluetooth configuration files
    The following configuration files are added.
    audio.conf, auto_pairing.conf, blacklist.conf main.le.conf
    
    Change-Id: If69c57308696f3c5549e595dbc48b00662b15051

--- msm8960.mk
+++ msm8960.mk
@@ -14,6 +14,13 @@ PRODUCT_COPY_FILES += \
     frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
     frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
 
+# Bluetooth configuration files
+PRODUCT_COPY_FILES += \
+    system/bluetooth/data/audio.conf:system/etc/bluetooth/audio.conf \
+    system/bluetooth/data/auto_pairing.conf:system/etc/bluetooth/auto_pairing.conf \
+    system/bluetooth/data/blacklist.conf:system/etc/bluetooth/blacklist.conf \
+    system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf \
+
 PRODUCT_PACKAGES += \
     battery_monitor \
     battery_shutdown
