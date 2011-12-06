f5741d78ce75c17cd81241597bec177cd0d623d6
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2011-12-06 15:49:24 +0530

    build: Cleanup/fix usage of ALL_PREBUILT
    ALL_PREBUILT usage is deprecated in ICS. To be in sync
    with AOSP Build System, replace the use of ALL_PREBUILT
    with BUILD_PREBUILT.
    
    Change-Id: I8f959bb8d886ba55809f7aeb56d91b45037c1025
    CRs-Fixed: 314291

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -51,10 +51,12 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
 include $(BUILD_PREBUILT)
 
-file := $(TARGET_OUT)/etc/init.qcom.mdm_links.sh
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/init.qcom.mdm_links.sh | $(ACP)
-	$(transform-prebuilt-to-target)
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.mdm_links.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
 
 ifeq ($(strip $(BOARD_HAS_QCOM_WLAN)),true)
 include $(CLEAR_VARS)
