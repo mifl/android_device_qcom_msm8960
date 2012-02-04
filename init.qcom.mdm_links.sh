28b80a75948adf19b0fb4cb29df9c38b47e2679b
Shashank Mittal <mittals@codeaurora.org>
2012-02-03 22:34:59 -0800

    Fusion 3: Add support to create links for efs images.
    Change-Id: Ib5aa4992b024bbe7fddd7d434d3dfd43661e6d17

--- init.qcom.mdm_links.sh
+++ init.qcom.mdm_links.sh
@@ -43,6 +43,9 @@ ln -s $MDM_IMAGES/dsp3.mbn /system/etc/firmware/dsp3.mbn 2>/dev/null
 ln -s $MDM_IMAGES/rpm.mbn  /system/etc/firmware/rpm.mbn  2>/dev/null
 ln -s $MDM_IMAGES/sbl1.mbn /system/etc/firmware/sbl1.mbn 2>/dev/null
 ln -s $MDM_IMAGES/sbl2.mbn /system/etc/firmware/sbl2.mbn 2>/dev/null
+ln -s $MDM_IMAGES/efs1.mbn /system/etc/firmware/efs1.mbn 2>/dev/null
+ln -s $MDM_IMAGES/efs2.mbn /system/etc/firmware/efs2.mbn 2>/dev/null
+ln -s $MDM_IMAGES/efs3.mbn /system/etc/firmware/efs3.mbn 2>/dev/null
 
 mount -t ext4 -o remount,ro,barrier=0 /dev/block/mmcblk0p12 /system
 
