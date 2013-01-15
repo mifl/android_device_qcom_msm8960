f8f0cf962cd7b62d806d4588d0b41e1fd6546b40
Jeff Johnson <jjohnson@codeaurora.org>
2013-01-15 10:31:19 -0800

    Update hostapd config file handling
    For JB MR1 we do not want to install default versions of hostapd data
    files in either the data or persist partitions.  Instead all default
    data files should first start in the system partition, and the
    appropriate runtime files will be created on the data partition by the
    SoftAP SDK framework as needed.
    
    Change-Id: I8bc7dfc89f5c1ec574ffdb54afdee2e2d28196e7

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -92,15 +92,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := hostapd_default.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(PRODUCT_OUT)/persist/qcom/softap
-LOCAL_SRC_FILES    := hostapd.conf
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := hostapd.conf
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_DATA)/misc/wifi
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd.conf
 include $(BUILD_PREBUILT)
 
@@ -108,7 +100,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := hostapd.accept
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_DATA)/hostapd
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd.accept
 include $(BUILD_PREBUILT)
 
@@ -116,7 +108,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := hostapd.deny
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH  := $(TARGET_OUT_DATA)/hostapd
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/hostapd
 LOCAL_SRC_FILES    := hostapd.deny
 include $(BUILD_PREBUILT)
 
