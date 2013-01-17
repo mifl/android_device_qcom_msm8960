1b6baed70c85ac51fcdfa783ccdcabe3d6322e52
Ajay Dudani <adudani@codeaurora.org>
2013-01-16 23:52:56 -0800

    msm8960: Add maxcpus=2 to the kernel command line
    Booting with more than two CPUs has only a negligible impact
    on boot time but significantly increases power consumption
    and temperature, especially until thermal management drivers
    have initialized. Set maxcpus=2 for better thermal and power
    efficiency during boot on 8064, which has four CPU cores.
    
    Change-Id: Ib898ecdc7ed466a3d3f68e1b5e759aee078e935d
    CRs-Fixed: 374783

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@ TARGET_NO_BOOTLOADER := true
 
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=msm8960
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=msm8960 maxcpus=2
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 BOARD_USES_ALSA_AUDIO:= true
