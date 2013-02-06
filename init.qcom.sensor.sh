889bda41a92ea78222eb784d4ae2bab5561e1383
Wentao Xu <wentaox@codeaurora.org>
2013-02-06 11:15:20 -0500

    init: update sensor init script for MSM8x30 targets
    Expand native sensor support to all MSM8x30 target variants
    (MSM8x30/AA/AB)
    
    Change-Id: Ib2a8688c5a5864a64871ef8d35c713044607c2b6

--- init.qcom.sensor.sh
+++ init.qcom.sensor.sh
@@ -42,7 +42,7 @@ fi
 # create symlink to target-specific config file
 platformid=`cat /sys/devices/system/soc/soc0/id`
 case "$platformid" in
-    "116" | "142" | "154") #msm8930
+    "116" | "117" | "118" | "119" | "142" | "143" | "144" | "154" | "155" | "156" | "157" | "179" | "180" | "181")
     ln -s /system/lib/hw/sensors.msm8930.so $SENSOR_HAL_SYMLINK 2>/dev/null
     ;;
 
