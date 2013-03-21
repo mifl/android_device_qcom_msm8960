c05ecaa81a8082ae297ec5d1174623f416c36856
Ming-yi Lin <mylin@codeaurora.org>
2013-03-21 10:38:57 +0800

    wlan: Use different wpa_supplicant.conf for wcn and ath6kl
    init.qcom.wifi.sh make wpa_supplicant.conf a symbolic in
    runtime. To keep this mechanism work across different branches,
    copy one wpa_supplicant.conf into wpa_supplicant_wcn.conf
    to avoid errors.
    
    Change-Id: Ic3cac5fc31a112f4a57e653c4a7efcee783d2102

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -88,6 +88,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := wpa_supplicant_wcn.conf
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := $(LOCAL_MODULE)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := hostapd_default.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
