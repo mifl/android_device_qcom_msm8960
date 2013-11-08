016079734db1bd6852cc68da31a1aab4683db72a
Narendra Babu Atmakuri <natmak@codeaurora.org>
2013-11-08 10:38:36 -0800

    Change cmdline to disable Uart logs.
    This is for boot time optimization and saves ~2 seconds.
    
    Change-Id: I86a7fe55a070f4c8dd04176cf3ca0f40b30a08bc

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -52,7 +52,11 @@ TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2
+### ttyHSL0 for LIQUID & CDP
+# BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2
+### ttyHSL1 for AUTOMOTIVE_CUSTOMER_PLATFORM1
+# BOARD_KERNEL_CMDLINE := console=ttyHSL1,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_EGL_CFG := device/qcom/$(TARGET_PRODUCT)/egl.cfg
 
 BOARD_BOOTIMAGE_PARTITION_SIZE := 23068672 # 22M
